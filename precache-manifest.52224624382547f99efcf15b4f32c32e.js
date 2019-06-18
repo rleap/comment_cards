f168f8e024ed6825462cb5978645a78eb7610608
Robert Leap <robertleap@gmail.com>
2019-06-17 22:09:42 -0700

    Updates

---
+++ precache-manifest.52224624382547f99efcf15b4f32c32e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a800aee90bd9129dc514faf2dd0267a0",
+    "url": "/comment_cards/index.html"
+  },
+  {
+    "revision": "92cda067c4bec7226b50",
+    "url": "/comment_cards/static/js/2.4d70910c.chunk.js"
+  },
+  {
+    "revision": "2f53ca4e2e79ddf5ff87",
+    "url": "/comment_cards/static/js/main.ae6aee6a.chunk.js"
+  },
+  {
+    "revision": "2d84936e3c8a56599861",
+    "url": "/comment_cards/static/js/runtime~main.c8a2eeb2.js"
+  }
+]);
