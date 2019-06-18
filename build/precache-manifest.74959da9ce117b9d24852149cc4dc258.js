50e3f61a9a982d66e3fb93ffa39e28e130bf57e2
Robert Leap <robertleap@gmail.com>
2019-06-17 21:24:34 -0700

    updated firebase info

---
+++ build/precache-manifest.74959da9ce117b9d24852149cc4dc258.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8b21624a9c1d72bee2bb78226f7f62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92cda067c4bec7226b50",
+    "url": "/static/js/2.4d70910c.chunk.js"
+  },
+  {
+    "revision": "2f53ca4e2e79ddf5ff87",
+    "url": "/static/js/main.ae6aee6a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
